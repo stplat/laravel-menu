1818d406441b7fa838d0b15c37e6d8bee3c94373
stsp <st.platonov@gmail.com>
2022-05-28 13:11:21 +0300

    fixes

---
+++ src/MenuServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Stsp\LaravelMenu;
+
+use Stsp\LaravelMenu\Console\UnpackCommand;
+use Illuminate\Support\ServiceProvider;
+
+
+class MenuServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->singleton(
+            'command.laravel-menu.unpack',
+            function ($app) {
+                return new UnpackCommand();
+            }
+        );
+
+        $this->commands('command.laravel-menu.unpack');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+    }
+}
